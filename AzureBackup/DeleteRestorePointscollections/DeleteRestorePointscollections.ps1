8684c8ab1056173a3b2f193fea9c006d040e0d0e
Oaker Min <7734956+Brucius@users.noreply.github.com>
2020-02-24 10:04:15 +0800

    Update DeleteRestorePointscollections.ps1
    Formatting changes

--- AzureBackup/DeleteRestorePointscollections/DeleteRestorePointscollections.ps1
+++ AzureBackup/DeleteRestorePointscollections/DeleteRestorePointscollections.ps1
@@ -1,33 +1,31 @@
-workflow Delete-RestorePointsCollections{
+workflow Delete-RestorePointsCollections {
     param([string[]]$Rgname)
     $resources = Get-AzureRmResource -ResourceGroupName "$Rgname"
     
      
     foreach -Parallel ($resource in $resources) {
-    $retry = $true
-    while ($retry){
-    Try
-    {
-    Write-Output "Removing $($resource.Name)"
-    $removeOps= Remove-AzureRmResource -ResourceId $resource.ResourceId -Force 
-    if ($removeOps) {$retry = $false}
-    }
+        $retry = $true
+        while ($retry) {
+            Try {
+                Write-Output "Removing $($resource.Name)"
+                $removeOps = Remove-AzureRmResource -ResourceId $resource.ResourceId -Force 
+                if ($removeOps) { $retry = $false }
+            }
      
-    Catch
-    {
-    $retry = $true
-    }
+            Catch {
+                $retry = $true
+            }
      
-    Finally {if ($retry) {write-output "Failed to delete $($resource.Name) --> Retrying"} else {write-output "Deletion of  $($resource.Name) successfull"}}
+            Finally { if ($retry) { write-output "Failed to delete $($resource.Name) --> Retrying" } else { write-output "Deletion of  $($resource.Name) successfull" } }
      
+        }
     }
-    }
-    }
+}
     
 #Parameters
-    $SubcriptionName = "Subscription Name"
-    $ResourceGroupname = "Resource group Name"
- #Run the workflow
-    Login-AzureRmAccount
-    Select-AzureRmSubscription -Subscription $SubcriptionName
-    Delete-RestorePointsCollections -Rgname $ResourceGroupname
+$SubcriptionName = "Subscription Name"
+$ResourceGroupname = "Resource group Name"
+#Run the workflow
+Login-AzureRmAccount
+Select-AzureRmSubscription -Subscription $SubcriptionName
+Delete-RestorePointsCollections -Rgname $ResourceGroupname
